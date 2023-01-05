cf1f5e410d4aebb02aa76564b38ad610063f5244
LakshikaDias <lakshika9504@gmail.com>
2023-01-05 13:23:54 +0530

    create side bar component

---
+++ src/components/Sidebar.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+
+const Sidebar = () => {
+  let navigate = useNavigate();
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Button variant="outline-primary">Compose</Button>
+        </Row>
+        <Row>
+          <Button
+            onClick={() => {
+              navigate("/mail/inbox");
+            }}
+            variant="outline-primary"
+          >
+            Inbox
+          </Button>
+        </Row>
+        <Row>
+          <Button variant="outline-primary">Starred</Button>
+        </Row>
+        <Row>
+          <Button variant="outline-primary">Snooozed</Button>
+        </Row>
+        <Row>
+          <Button
+            variant="outline-primary"
+            onClick={() => {
+              navigate("/mail/sent");
+            }}
+          >
+            Sent
+          </Button>
+        </Row>
+        <Row>
+          <Button
+            variant="outline-primary"
+            onClick={() => {
+              navigate("/mail/draft");
+            }}
+          >
+            Drafts
+          </Button>
+        </Row>
+        <Row>
+          <Button
+            variant="outline-primary"
+            onClick={() => {
+              navigate("/mail/spam");
+            }}
+          >
+            Spam
+          </Button>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default Sidebar;
